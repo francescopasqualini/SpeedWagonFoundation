da4ef4da55baf8b7c2c542bfadbd195a022de283
pasqui <francesco.pasquali-1@studenti.unitn.it>
2019-10-16 13:10:09 +0000

    gitinit file

---
+++ gitinit.sh
@@ -0,0 +1,6 @@
+echo "# SpeedWagonFoundation" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/francescopasqualini/SpeedWagonFoundation.git
+git push -u origin master

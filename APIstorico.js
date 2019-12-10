78ae63a960e5bc2aba564219feab3fab70e8fbfc
Francesco Pasqualini <francesco.pasquali-1@studenti.unitn.it>
2019-12-10 21:56:43 +0100

    Added DELETE method

--- APIstorico.js
+++ APIstorico.js
@@ -1,3 +1,7 @@
+/* TODO:
+ *  change status code 404 to 'Malformed input' code
+ **/
+
 var express = require('express');
 var app = express();
 
@@ -45,9 +49,9 @@ app.post('/storico', function(req, res){
   console.log('POST storico');
   let code = 503;
   let json = req.body;
-  var username = json["username"];
-  var data = json["data"];
-  var esercizi = json["esercizi"];
+  let username = json["username"];
+  let data = json["data"];
+  let esercizi = json["esercizi"];
   if (username && data && esercizi){
     if (!storicoDB[username]){
       storicoDB[username] = {}
@@ -77,9 +81,9 @@ app.post('/storico', function(req, res){
 app.get('/storico', function(req, res){
     console.log('GET storico');
     let json = req.body;
-    var username = json["username"];
-    var data_inizio = json["data_inizio"];
-    var data_fine = json["data_fine"];
+    let username = json["username"];
+    let data_inizio = json["data_inizio"];
+    let data_fine = json["data_fine"];
     console.log('DONE');
     // if (username && data_inizio && data_fine){
         
@@ -89,34 +93,72 @@ app.get('/storico', function(req, res){
 
 // update data PUT
 app.put('/storico', function(req, res){
-    console.log('PUT storico');
-    let json = req.body;
-    var username = json["username"];
-    var data = json['data'];
-    var update = json['update'];
-    if (username && data && update){
-      if (storicoDB[username][data]){
-        for (var key in update){
-          var val = storicoDB[username][data][key];
-          if (update == 0){
-            delete update[key];
-          } else if (val) {
-            val = val + update[key];
-          } else if (update[key] > 0){
-            storicoDB[username][data][key] = update[key];
-          } else {
-            console.log('Error. Unexpected value: ' + key + ' ' + update[key]);
-          }
+  console.log('PUT storico');
+  let code = 503;
+  let json = req.body;
+  let username = json["username"];
+  let data = json['data'];
+  let update = json['update'];
+  if (username && data && update){
+    if (storicoDB[username][data]){
+      for (var key in update){
+        var val = storicoDB[username][data][key];
+        if (update == 0){
+          delete update[key];
+        } else if (val) {
+          // if key exists
+          storicoDB[username][data][key] += update[key];
+        } else if (update[key] > 0){
+          // if key not exists
+          storicoDB[username][data][key] = update[key];
+        } else {
+          console.log('Error. Unexpected value: ' + key + ' ' + update[key]);
         }
       }
+    }
+    res.response={
+      error : null,
+      done : 'Update completed.'
+    };
+    code = 200;
+  } else {
+    res.response={
+      error : 'Error. Username or data or esercizi are INVALID'
+    };
+    code = 404;
+  }
+  res.status(code)
+  res.json(res.response);
+});
+
+// delete data DELETE
+app.delete('/storico', function(req, res){
+  console.log('DELETE storico');
+  let code = 503;
+  let json = req.body;
+  let username = json["username"];
+  let data_to_del = json['data_to_del'];
+  if (username && data_to_del){
+    if (storicoDB[username][data_to_del]){
+      delete storicoDB[username][data_to_del];
       res.response={
         error : null,
-        done : 'Update completed.'
-      }
+        delete: 'Del completed'
+      };
+      code = 200;
     } else {
       res.response={
-        error : 'Error. Username or data or esercizi are INVALID'
+        error : null,
+        delete: 'Data to del NOT FOUND'
       };
+      code = 404;
     }
-    res.json(res.response);
+  } else {
+    res.response={
+      error : 'Error. Username or data to del are INVALID'
+    };
+    code = 404;
+  }
+  res.status(code);
+  res.json(res.response);
 });

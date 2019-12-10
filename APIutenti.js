c121ff767d917790cb75f56e0c9b98e921262b31
JacopoSitran <jacopo.sitran@gmail.com>
2019-12-10 16:35:31 +0100

    aggiunta la put con controllo input

--- APIutenti.js
+++ APIutenti.js
@@ -11,43 +11,6 @@ app.listen(port, function() {
 });
 var badFormed = {error:"request bad formed"}
 
-let merge = (...arguments) => {
-
-    // Variables
-    let target = {};
-  
-    // Merge the object into the target object
-    let merger = (obj) => {
-        for (let prop in obj) {
-       if (obj.hasOwnProperty(prop)) {
-           if (Object.prototype.toString.call(obj[prop]) === '[object Object]') {
-          // If we're doing a deep merge and the property is an object
-          target[prop] = merge(target[prop], obj[prop]);
-           } else {
-          // Otherwise, do a regular merge
-          target[prop] = obj[prop];
-           }
-       }
-        }
-    };
-  
-   //Loop through each object and conduct a merge
-   for (let i = 0; i < arguments.length; i++) {
-       merger(arguments[i]);
-   }
-  
-   return target;
-  };
-
-  function mergeArrayObjects(arr1,arr2){
-    return arr1.map((item,i)=>{
-       if(item.id === arr2[i].id){
-           //merging two objects
-         return merge(item,arr2[i])
-       }
-    })
-  }
-  
 
 
 //qui metto tutta la parte del "DB"
@@ -182,164 +145,31 @@ app.put('/users/:id', function(req,res){
     const id = parseInt(req.params.id);
     let payload = req.body
     let utente = utentiDatabase[id];
-    
     if (utente === undefined) {
         res.status(404).send()
     } else {
-        Object.keys(payload).forEach(element => {
-            utentiDatabase[id][element]=payload[element]
-        });
-        res.status(200).send(utentiDatabase[id])
+        if ((Object.keys(payload).filter(x => !["name","surname","username","password","email","isPt"].includes(x))).length > 0) {
+            console.log("BAD 1 ")
+            res.status(400).json(badFormed)
+        }else{
+            var flag = false
+            Object.keys(payload).forEach(element => {
+                if(!["name","surname","username","password","email","isPt"].includes(element)){
+                    flag=true
+                    console.log(payload[element])
+                }
+            });
+            if (flag == true) {
+                res.status(400).json(badFormed)
+                console.log("bad 2")
+            } else {
+                Object.keys(payload).forEach(element => {
+                    utentiDatabase[id][element]=payload[element]
+                });
+                res.status(200).send(utentiDatabase[id])
+            }
+            
+        }
     }
   });
 
-
-//API Delete DELETE
-//input : ide della scheda
-//output : "cancello" la scheda e restutisco boh
-/*
-app.get('/schede/delete',function(req,res){
-    console.log('API_5');
-    let id = req.query.id;
-    //togli tale item dal "DB"
-    //restituisci : yeah l'ho tolto
-})
-*/
-
-
-
-
-//vecchie cose
-/*
-{
-    "username": "username",
-    "esercizi":[
-        [
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            ...
-        ],
-        [
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            ...
-        ],
-        ...
-    ]
-}
-*/
-
-                  /*
-var SchedeDatabase={
-  scheda1:{
-    username: "Luca",
-    id : 1,
-    nome : "panca piana",
-    tempo_recupero : 120,
-    peso : 20,
-    nserie : 4,
-    nripetizioni : 90,
-    descrizione : "usa la panca piana"
-  },
-  scheda2:{
-    username: "Federico",
-    id : 2,
-    nome : "addominali",
-    tempo_recupero : 240,
-    peso : 10,
-    nserie : 7,
-    nripetizioni : 10,
-    descrizione : "fai degli addominali"
-  },
-  scheda3:{
-    username: "Sara",
-    id : 3,
-    nome : "flessioni",
-    tempo_recupero : 60,
-    peso : 20,
-    nserie : 2,
-    nripetizioni : 20,
-    descrizione : "fai delle flessioni"
-  }
-}*/
-/*
-var schede = {
-    "username": "username",
-    "esercizi":[
-        [
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            ...
-        ],
-        [
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            {
-                "id" : 0,
-                "nome" : "nome",
-                "tempo_recupero": 0,
-                "peso": 0,
-                "nserie": 0,
-                "nripetizioni": 0,
-                "descrizione": "descrizione"
-            },
-            ...
-        ],
-        ...
-    ]
-*/

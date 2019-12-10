afbe1b4b8b7a54af0f51db93f4cd5b3bad33ada3
Alessio Valenza <alessio.valenza@studenti.unitn.it>
2019-12-10 22:30:05 +0100

    Aggiunto middleware per check sui parametri. aggiunto logger. Alcune modifiche alle api

--- chat.js
+++ chat.js
@@ -1,100 +1,122 @@
-const classes = require('./classes');
-const Db = classes.Db;
-const Message = classes.Message;
+const classes = require('./classes')
+const Db = classes.Db
+const Message = classes.Message
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-var db = new Db();
+var db = new Db()
 
-app.use(bodyParser.json()); 
+const MessageTypes = ['text', 'image', 'voicemail', 'video']
+
+app.use(bodyParser.json()) 
 
 app.listen(port, function () {
-    console.log('Listening at http://localhost:' + port);
+    console.log('Listening at http://localhost:' + port)
+})
+
+app.use('/chat/:idFrom/:idTo/:idMessage?', function (req, res, next) {
+    
+    const idFrom = Number.parseInt(req.params.idFrom)
+    const idTo = Number.parseInt(req.params.idTo)
+    const idMessage = Number.parseInt(req.params.idMessage)
+    let idCheck = Number.isInteger(idFrom) && Number.isInteger(idTo)
+    let messageCheck = idMessage ? Number.isInteger(idMessage) : true
+
+    if (idCheck && messageCheck){
+        res.locals.idFrom = idFrom 
+        res.locals.idTo = idTo 
+        res.locals.idMessage = idMessage 
+        next()
+    }else{
+        res.status(400).send()
+    }
 });
+
+//logger
+app.use('/chat/:idFrom/:idTo/:idMessage?', function (req, res, next) {
+    console.log(`${req.method} : ${req.originalUrl}`)
+    next()
+})
+
+
+//apis
 
 app.get('/chat', function (req, res) {
-   res.status(200);
-   res.send(db); 
-});
+   res.status(200)
+   res.json(db) 
+})
 
 app.get('/chat/:idFrom/:idTo/:idMessage', function(req, res){
-    
-    const idFrom = parseInt(req.params.idFrom);
-    const idTo = parseInt(req.params.idTo);
-    const idMessage = parseInt(req.params.idMessage);
-    console.log("idFrom: " + idFrom + "\nidTo: " + idTo + "\nidMessage: " + idMessage);
-    let message = db.retrieveMessage(idFrom, idTo, idMessage);
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    const idMessage = res.locals.idMessage
+    let message = db.retrieveMessage(idFrom, idTo, idMessage)
     if (message){
-        res.status(200);
-        res.send(message);
+        res.status(200).json(message)
     }else{
-        res.status(404);
-        res.send();
+        res.status(404).send()
     }
     
-});
+})
 
 app.get('/chat/:idFrom/:idTo', function (req, res) {
-    
-    const idFrom = parseInt(req.params.idFrom);
-    const idTo = parseInt(req.params.idTo);
-    console.log("idFrom: " + idFrom + "\nidTo: " + idTo);
-    let chat = db.retrieveChat(idFrom, idTo);
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    let chat = db.retrieveChat(idFrom, idTo)
     if (chat){
-        res.status(200);
-        res.send(chat);
+        res.status(200).json(chat)
     }else{
-        res.status(404);
-        res.send();
+        res.status(404).send()
     }
     
-});
+})
 
-app.post('/chat/:idFrom/:idTo', function(req, res){
-    
-    const idFrom = parseInt(req.params.idFrom);
-    const idTo = parseInt(req.params.idTo);
-    let message = new Message();
-    let jsonResp = req.body;
-    if (jsonResp && jsonResp.id && jsonResp.body){
-        message.id = parseInt(jsonResp.id);
-        message.body = jsonResp.body;
-        db.insertMessage(idFrom, idTo, message);
-        res.status(201);
+app.post('/chat/:idFrom/:idTo', function (req, res) {
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    let body = req.body
+    if (body && body.content && MessageTypes.includes(body.type)){
+        let message = new Message(body.type, idFrom, idTo, body.content)
+        if (db.sendMessage(idFrom, idTo, message)){
+            res.status(201).send()
+        }else {
+            res.status(500).send()
+        }
     }else{
-        res.status(400);
+        res.status(400).send()
     }
-    
-    res.send();
-});
+})
+
 
 app.delete('/chat/:idFrom/:idTo/:idMessage', function (req, res) {
-    
-    const idFrom = parseInt(req.params.idFrom);
-    const idTo = parseInt(req.params.idTo);
-    const idMessage = parseInt(req.params.idMessage);
-    console.log("idFrom: " + idFrom + "\nidTo: " + idTo + "\nidMessage: " + idMessage);
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    const idMessage = res.locals.idMessage
     if (db.deleteMessage(idFrom, idTo, idMessage)) {
-        res.status(201);
+        res.status(200).send()
     }else{
-        res.status(404);
+        res.status(404).send()
     }
-    res.send();
-});
+})
 
 app.put('/chat/:idFrom/:idTo/:idMessage', function (req, res) {
-    const idFrom = parseInt(req.params.idFrom);
-    const idTo = parseInt(req.params.idTo);
-    let jsonResp = req.body;
-    if (jsonResp && jsonResp.id && jsonResp.body){
-        db.editMessage(idFrom, idTo, jsonResp);
-        res.status(201);
-    }else{
-        res.status(404);
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    const idMessage = res.locals.idMessage
+    let body = req.body
+    if (body && body.content){
+        body.id = idMessage
+        if (db.editMessage(idFrom, idTo, body)){
+            res.status(201).send()
+        }else{
+            res.status(404).send()
+        }
+    } else {
+        res.status(400).send()
     }
-    res.send();
-});
+})
+
--- classes.js
+++ classes.js
@@ -1,78 +1,105 @@
 class Db {
     
     constructor() {
-        this.dbchat = {};
+        this.dbchat = {}
+        this.dbchat.nextId = 1
+        this.dbchat.users = new Set()
     }
 
-    insertMessage(idUser1, idUser2, message) {
-        let idchat = Math.min(idUser1, idUser2).toString() + ":" + Math.max(idUser1, idUser2).toString();
+    sendMessage(idUser1, idUser2, message) {
+        let res = true
+        this.dbchat.users.add(idUser1)
+        this.dbchat.users.add(idUser2)
+        let idchat = getIdChat(idUser1, idUser2)
+        message.id = this.dbchat.nextId ++
+        message.sent_at = getTimestamp()
         if (idchat in this.dbchat) {
-            this.dbchat[idchat][idUser1].push(message);
-        } else {
-            this.dbchat[idchat] = {};
-            this.dbchat[idchat][idUser1] = [];
-            this.dbchat[idchat][idUser2] = [];
-            this.dbchat[idchat][idUser1].push(message);
+            this.dbchat[idchat][idUser1].push(message)
+        }else {
+            this.dbchat[idchat] = {}
+            this.dbchat[idchat][idUser1] = []
+            this.dbchat[idchat][idUser2] = []
+            this.dbchat[idchat][idUser1].push(message)
         }
+        return res
+    }
+    sendAll(idUser1, message){
+
     }
     retrieveMessage(idSender, idUser2, idMessage) {
-        let res = null;
-        let idchat = Math.min(idSender, idUser2).toString() + ":" + Math.max(idSender, idUser2).toString();
+        let res = null
+        let idchat = getIdChat(idSender, idUser2)
         if (idchat in this.dbchat) {
-            var messagesList = this.dbchat[idchat][idSender];
-            var index = messagesList.findIndex(m => m.id == idMessage);
-            res = index != -1 ? messagesList[index] : res;
+            var messagesList = this.dbchat[idchat][idSender]
+            var index = messagesList.findIndex(m => m.id == idMessage)
+            res = index != -1 ? messagesList[index] : res
+            res.received_at = getTimestamp()
         }
-        return res;
+        return res
     }
     retrieveChat(idSender, idUser2) {
-        let res = null;
-        let idchat = Math.min(idSender, idUser2).toString() + ":" + Math.max(idSender, idUser2).toString();
+        let res = null
+        let idchat = getIdChat(idSender, idUser2)
         if (idchat in this.dbchat){
-            res = this.dbchat[idchat];
+            res = this.dbchat[idchat]
         }
-        return res;
+        return res
     }
     deleteMessage(idSender, idUser2, idMessage) {
-        let res = false;
-        let idchat = Math.min(idSender, idUser2).toString() + ":" + Math.max(idSender, idUser2).toString();
+        let res = false
+        let idchat = getIdChat(idSender, idUser2)
         if (idchat in this.dbchat) {
-            let messagesList = this.dbchat[idchat][idSender];
-            let index = messagesList.findIndex(m => m.id == idMessage);
+            let messagesList = this.dbchat[idchat][idSender]
+            let index = messagesList.findIndex(m => m.id == idMessage)
             if (index != -1){
-                messagesList.splice(index, 1);
-                res = true;
+                messagesList.splice(index, 1)
+                res = true
             }
         }
-        return res;
+        return res
     }
     editMessage(idSender, idUser2, message){
-        let res = false;
-        let idchat = Math.min(idSender, idUser2).toString() + ":" + Math.max(idSender, idUser2).toString();
+        let res = false
+        let idchat = getIdChat(idSender, idUser2)
         if (idchat in this.dbchat) {
-            let messagesList = this.dbchat[idchat][idSender];
-            let index = messagesList.findIndex(m => m.id == message.id);
+            let messagesList = this.dbchat[idchat][idSender]
+            let index = messagesList.findIndex(m => m.id == message.id)
             if (index != -1) {
-                let m = messagesList[index];
-                m.body = message.body; //modifico il body del messaggio
-                m.edited_at = new Date().getTime();
-                res = true;
+                let m = messagesList[index]
+                m.content = message.content //modifico il content del messaggio
+                m.edited_at = getTimestamp()
+                res = true
             }
         }
-        return res;
+        return res
     }
-};
+}
 
 class Message{
     
-    constructor(type, id, from, to, in_response_to, body){
-        this.type = type;
-        this.id = id;
-        this.from = from;
-        this.to = to;
-        this.in_response_to = in_response_to;
-        this.body = body;
+    constructor(type, from, to, content, sent_at){
+        this.type = type
+        this.from = from
+        this.to = to
+        this.content = content
+        this.sent_at = sent_at
     }
-};
+}
 
-module.exports = {Db, Message};
+function getTimestamp() {
+    let date = new Date()
+    let res = date.getFullYear().toString() + "-" +
+        date.getMonth().toString() + "-" +
+        date.getDay().toString() + " " +
+        date.getHours().toString() + ":" +
+        date.getMinutes().toString() + ":" +
+        date.getSeconds().toString() + ":" +
+        date.getMilliseconds().toString()
+    return res
+}
+
+function getIdChat(id1, id2){
+    return Math.min(id1, id2).toString() + ":" + Math.max(id1, id2).toString()
+}
+
+module.exports = {Db, Message}

6d40e96bd6ee7471b2b40b0db7ddfc13cb4bf435
Francesco Pasqualini <francesco.pasquali-1@studenti.unitn.it>
2019-12-10 18:09:35 +0100

    Added put API

--- APIstorico.js
+++ APIstorico.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.json);
-const port = process.env.PORT || 3000;
 
-app.listen(port, function(){
-  console.log('Server running on port ', port);
-});
 
 // Database
 //// change date with timestamp of the day
@@ -38,27 +34,74 @@ app.post('/storico', function(req, res){
   var data = json["data"];
   var esercizi = json["esercizi"];
   if (username && data && esercizi){
-      storicoDB[username][data] = esercizi;
-      res.response={
-          error: null,
-          done: "Added " + esercizi.length + "exercise"
-      }
+    storicoDB[username][data] = esercizi;
+    res.response={
+      error: null,
+      done: "Added " + esercizi.length + "exercise"
+    }
   } else {
     res.response={
-        error: "Error. Username or data or esercizi are INVALID"
+      error: "Error. Username or data or esercizi are INVALID"
     };
   }
   res.json(res.response);
 });
 
-// get storico ET
+// get storico GET
 app.get('/storico', function(req, res){
     console.log('GET storico');
     let json = req.body;
     var username = json["username"];
     var data_inizio = json["data_inizio"];
     var data_fine = json["data_fine"];
-    if (username && data_inizio && data_fine){
+    console.log('DONE');
+    // if (username && data_inizio && data_fine){
         
+    // }
+    res.response={error:"Not pervenuto"};
+});
+app.get('/', function(req, res){ 
+  console.log("GELLO")
+  res.send('Hello World!');
+});
+
+// update data PUT
+app.put('/storico', function(req, res){
+    console.log('PUT storico');
+    let json = req.body;
+    var username = json["username"];
+    var data = json['data'];
+    var update = json['update'];
+    if (username && data && update){
+      if (storicoDB[username][data]){
+        for (var key in update){
+          var val = storicoDB[username][data][key];
+          if (update == 0){
+            delete update[key];
+          } else if (val) {
+            val = val + update[key];
+          } else if (update[key] > 0){
+            storicoDB[username][data][key] = update[key];
+          } else {
+            console.log('Error. Unexpected value: ' + key + ' ' + update[key]);
+          }
+        }
+      }
+      res.response={
+        error : null,
+        done : 'Update completed.'
+      }
+    } else {
+      res.response={
+        error : 'Error. Username or data or esercizi are INVALID'
+      };
     }
+    res.json(res.response);
+});
+
+const port = process.env.PORT || 3000;
+
+var server = app.listen(port, '127.0.0.1', function(){
+  var host = server.address().address;
+  console.log('Server running on port ', port, ' @ ', host);
 });

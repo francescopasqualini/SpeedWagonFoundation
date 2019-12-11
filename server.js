931da4f83daadeae31bec5560fe903d35d78fe49
Frapava <francesco.pavanello@studenti.unitn.it>
2019-12-11 18:49:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const port =  process.env.PORT ||  3000;
 
 app.use(express.json());
 
-app.listen(port, function() {
-  console.log('Server running on port ', port);
-});
-
 const infoRouter = require('./routes/APIInfo');
 app.use('/info', infoRouter);
 
@@ -16,3 +12,7 @@ app.get('/', function(req, res){
   res.status(200);
   res.json({sectionAvaible: "/info"})
 })
+
+app.listen(port, function() {
+  console.log('Server running on port ', port);
+});

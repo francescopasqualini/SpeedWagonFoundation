cc3faeb2473bef5a7742556d6167add3205c74ae
Frapava <francesco.pavanello@studenti.unitn.it>
2019-12-12 22:26:11 +0100

    fix

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const app = express();
 
 const port =  process.env.PORT ||  3000;
 
 app.use(express.json());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 const infoRouter = require('./routes/APIInfo');
 app.use('/info', infoRouter);

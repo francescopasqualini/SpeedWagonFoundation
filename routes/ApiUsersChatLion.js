574b93c582fd8b5d6a52958ac00aab15c745e252
Frapava <francesco.pavanello@studenti.unitn.it>
2019-12-12 22:13:39 +0100

    add files to routes

---
+++ routes/ApiUsersChatLion.js
@@ -0,0 +1,348 @@
+/******* configs ********/
+var express = require('express');
+const bodyParser = require('body-parser');
+const classes = require('./classes');
+const router = express.Router(); 
+
+const Message = classes.Message
+var db = classes.db
+
+const MessageTypes = ['text', 'image', 'voicemail', 'video']
+const badFormed = { error: "request bad formed" }
+const notFound = { error: "id not found" }
+const serverError = { error: "server error" }
+
+var utentiDatabase = {
+    1: {
+        id: 1,
+        password: "password",
+        name: "bruno",
+        surname: "crispo",
+        email: "example@mail.com",
+        username: "processo",
+        isPt: true,
+    },
+    2: {
+        id: 2,
+        password: "12345",
+        name: "gino",
+        surname: "perna",
+        email: "example@mail.com",
+        username: "mongodb",
+        isPt: false
+    }
+};
+var dim = 2;
+
+/*middlewares*/ 
+
+router.use('/chat/:idFrom/:idTo?/:idMessage?', function (req, res, next) {
+    const idFrom = Number.parseInt(req.params.idFrom)
+    const idTo = Number.parseInt(req.params.idTo)
+    const idMessage = Number.parseInt(req.params.idMessage)
+
+    let idFromCheck = Number.isInteger(idFrom)
+    let idToCheck = (req.params.idTo == undefined) || (Number.isInteger(idTo))
+    let idMessageCheck = (req.params.idMessage == undefined) || (Number.isInteger(idMessage))
+    let sameCheck = idFrom != idTo
+    let consistencyCheck = idFromCheck && idToCheck && idMessageCheck && sameCheck
+
+    let isIdFromInDb = idFrom in utentiDatabase
+    let isIdToInDb = (req.params.idTo == undefined) || (idTo in utentiDatabase)
+    let isInDbCheck = isIdFromInDb && isIdToInDb
+
+    console.log("utentidb: " + Object.keys(utentiDatabase))
+
+    if (consistencyCheck && isInDbCheck) {
+        res.locals.idFrom = idFrom
+        res.locals.idTo = idTo
+        res.locals.idMessage = idMessage
+        next()
+    } else if (!consistencyCheck) {
+        res.status(400).json(badFormed)
+    } else {
+        res.status(404).json(notFound)
+    }
+});
+
+router.use('/chat/:idFrom/:idTo?/:idMessage?', function (req, res, next) {
+    console.log(`${req.method} : ${req.originalUrl}`)
+    next()
+})
+
+
+/********* /chat api **********/s
+
+/* Retrieve the whole dbchat. For testing purposes*/
+router.get('/chat', function (req, res) {
+    res.status(200)
+    res.json(db.dbchat)
+})
+
+//get a message
+router.get('/chat/:idFrom/:idTo/:idMessage', function (req, res) {
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    const idMessage = res.locals.idMessage
+    let message = db.retrieveMessage(idFrom, idTo, idMessage)
+    if (message) {
+        res.status(200).json(message)
+    } else {
+        res.status(404).json(notFound)
+    }
+
+})
+
+//get a chat
+router.get('/chat/:idFrom/:idTo', function (req, res) {
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    let chat = db.retrieveChat(idFrom, idTo)
+    if (chat) {
+        res.status(200).json(chat)
+    } else {
+        res.status(404).json(notFound)
+    }
+
+})
+
+//post a message (user to user)
+router.post('/chat/:idFrom/:idTo', function (req, res) {
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    let body = req.body
+    if (body && body.content && MessageTypes.includes(body.type)) {
+        let message = new Message(body.type, idFrom, idTo, body.content)
+        if (db.sendMessage(idFrom, idTo, message)) {
+            res.status(201).send()
+        } else {
+            res.status(500).json(serverError)
+        }
+    } else {
+        res.status(400).json(badFormed)
+    }
+})
+
+//post a message (user to all)
+router.post('/chat/:idFrom', function (req, res) {
+    const idFrom = res.locals.idFrom
+    let body = req.body
+    if (body && body.content && MessageTypes.includes(body.type)) {
+        let success = true
+        Object.keys(utentiDatabase).forEach(function (user) {
+            if (user != idFrom) {
+                let message = new Message(body.type, idFrom, user, body.content)
+                success = db.sendMessage(idFrom, user, message) && success
+            }
+        })
+        if (success) {
+            res.status(201).send()
+        } else {
+            res.status(500).json(serverError)
+        }
+    } else {
+        res.status(400).json(badFormed)
+    }
+})
+
+//delete a message
+router.delete('/chat/:idFrom/:idTo/:idMessage', function (req, res) {
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    const idMessage = res.locals.idMessage
+    if (db.deleteMessage(idFrom, idTo, idMessage)) {
+        res.status(204).send()
+    } else {
+        res.status(404).json(notFound)
+    }
+})
+
+//edit a message
+router.put('/chat/:idFrom/:idTo/:idMessage', function (req, res) {
+    const idFrom = res.locals.idFrom
+    const idTo = res.locals.idTo
+    const idMessage = res.locals.idMessage
+    let body = req.body
+    if (body && body.content) {
+        body.id = idMessage
+        if (db.editMessage(idFrom, idTo, body)) {
+            res.status(204).send()
+        } else {
+            res.status(404).json(notFound)
+        }
+    } else {
+        res.status(400).json(badFormed)
+    }
+})
+
+
+/********* /users api **********/
+
+router.get('/users', function (req, res) {
+    //console.log('API_1');
+    try {
+        let utentiDatabaseArray = []
+        Object.keys(utentiDatabase).forEach(element => {
+            utentiDatabaseArray.push(utentiDatabase[element])
+        });
+
+        var limit = req.query.limit
+        var offset = req.query.offset
+        //console.log(limit)
+        //console.log(offset)
+        if (limit == undefined) {
+            limit = 10
+        }
+        if (offset == undefined) {
+            offset = 0
+        }
+        let tmpArray = []
+        for (let index = offset; index < utentiDatabaseArray.length && index < (offset + limit); index++) {
+            // console.log(index)
+            //console.log(utentiDatabaseArray[index])
+            tmpArray.push(utentiDatabaseArray[index])
+
+        }
+        let risposta = {
+            results: tmpArray,
+            metadata: {
+                total: utentiDatabaseArray.length
+            }
+        }
+        res.status(200)
+        res.json(risposta);
+
+
+    } catch (error) {
+        res.status(500)
+        res.json(serverError)
+    }
+
+
+});
+
+router.get('/users/:id', function (req, res) {
+    try {
+
+        //console.log('API_2');
+        const id = parseInt(req.params.id);
+        let response = utentiDatabase[id]
+        //lettura dei parametri
+        if (response === undefined) {
+            //console.log("404")
+            res.status(404)
+            res.json(notFound)
+        }
+        else {
+            //console.log("200")
+            //console.log(response)
+            res.status(200).json(response);
+        }
+    } catch (error) {
+        res.status(500)
+        res.json(serverError)
+    }
+
+});
+
+
+
+//cacella un utente
+router.delete('/users/:id', function (req, res) {
+    try {
+        //console.log('API_3');
+        const id = parseInt(req.params.id);
+        let response = utentiDatabase[id]
+        if (response === undefined) {
+            //console.log(response)
+            res.status(404)
+            res.json(notFound)
+        } else {
+            let tmp = utentiDatabase[id]
+            delete utentiDatabase[id]
+            res.status(200);
+            res.json(tmp)
+        }
+    } catch (error) {
+        res.status(500)
+        res.json(serverError)
+    }
+
+});
+
+
+
+router.post('/users', function (req, res) {
+    try {
+        //console.log('API_4');
+        let payload = req.body;
+        if (payload["password"] === undefined || payload["name"] === undefined ||
+            payload["surname"] === undefined || payload["email"] === undefined ||
+            payload["isPt"] === undefined || Object.keys(payload).length != 6 || payload["username"] === undefined) {
+            res.status(400).json(badFormed)
+        }
+        else {
+            let key = dim + 1;
+            dim = dim + 1
+            key = key + "";
+            utentiDatabase[key] = {}
+            utentiDatabase[key]["id"] = dim
+            utentiDatabase[key]["password"] = "null"
+            utentiDatabase[key]["name"] = "nul404l"
+            utentiDatabase[key]["surname"] = "null"
+            utentiDatabase[key]["email"] = "null"
+            utentiDatabase[key]["username"] = "null"
+            utentiDatabase[key]["isPt"] = false
+            Object.keys(payload).forEach(element => {
+                utentiDatabase[key][element] = payload[element]
+            });
+            res.status(200).json(utentiDatabase[key])
+        }
+    } catch (error) {
+        res.status(500)
+        res.json(serverError)
+    }
+
+
+});
+
+router.put('/users/:id', function (req, res) {
+    try {
+        const id = parseInt(req.params.id);
+        let payload = req.body
+        let utente = utentiDatabase[id];
+        if (utente === undefined) {
+            res.status(404).json(notFound)
+        } else {
+            if ((Object.keys(payload).filter(x => !["name", "surname", "username", "password", "email", "isPt"].includes(x))).length > 0) {
+                //console.log("BAD 1 ")
+                res.status(400).json(badFormed)
+            } else {
+                var flag = false
+                Object.keys(payload).forEach(element => {
+                    if (!["name", "surname", "username", "password", "email", "isPt"].includes(element)) {
+                        flag = true
+                        //console.log(payload[element])
+                    }
+                });
+                if (flag == true) {
+                    res.status(400).json(badFormed)
+                    //console.log("bad 2")
+                } else {
+                    Object.keys(payload).forEach(element => {
+                        utentiDatabase[id][element] = payload[element]
+                    });
+                    res.status(200).json(utentiDatabase[id])
+                }
+
+            }
+        }
+    } catch (error) {
+        res.status(500)
+        res.json(serverError)
+    }
+
+});
+
+
+module.exports = router;
---
+++ routes/classes.js
@@ -0,0 +1,105 @@
+class Db {
+    
+    constructor() {
+        this.dbchat = {}
+        this.dbchat.nextId = 1
+    }
+
+    sendMessage(idUser1, idUser2, message) {
+        let res = true
+        let idchat = getIdChat(idUser1, idUser2)
+        message.id = this.dbchat.nextId ++
+        message.sent_at = getTimestamp()
+        if (idchat in this.dbchat) {
+            this.dbchat[idchat][idUser1].push(message)
+        }else {
+            this.dbchat[idchat] = {}
+            this.dbchat[idchat][idUser1] = []
+            this.dbchat[idchat][idUser2] = []
+            this.dbchat[idchat][idUser1].push(message)
+        }
+        return res
+    }
+    retrieveMessage(idSender, idUser2, idMessage) {
+        let res = null
+        let idchat = getIdChat(idSender, idUser2)
+        if (idchat in this.dbchat) {
+            var messagesList = this.dbchat[idchat][idSender]
+            var index = messagesList.findIndex(m => m.id == idMessage)
+            res = index != -1 ? messagesList[index] : res
+            if (res){
+                res.received_at = getTimestamp()
+            }
+        }
+        return res
+    }
+    retrieveChat(idSender, idUser2) {
+        let res = null
+        let idchat = getIdChat(idSender, idUser2)
+        if (idchat in this.dbchat){
+            res = this.dbchat[idchat]
+        }
+        return res
+    }
+    deleteMessage(idSender, idUser2, idMessage) {
+        let res = false
+        let idchat = getIdChat(idSender, idUser2)
+        if (idchat in this.dbchat) {
+            let messagesList = this.dbchat[idchat][idSender]
+            let index = messagesList.findIndex(m => m.id == idMessage)
+            if (index != -1){
+                messagesList.splice(index, 1)
+                res = true
+            }
+        }
+        return res
+    }
+    editMessage(idSender, idUser2, message){
+        let res = false
+        let idchat = getIdChat(idSender, idUser2)
+        if (idchat in this.dbchat) {
+            let messagesList = this.dbchat[idchat][idSender]
+            let index = messagesList.findIndex(m => m.id == message.id)
+            if (index != -1) {
+                let m = messagesList[index]
+                m.content = message.content //modifico il content del messaggio
+                m.edited_at = getTimestamp()
+                res = true
+            }
+        }
+        return res
+    }
+}
+
+class Message{
+    
+    constructor(type, from, to, content, sent_at){
+        this.type = type
+        this.from = from
+        this.to = to
+        this.content = content
+        this.sent_at = sent_at
+        this.received_at = null 
+        this.edited_at = null
+    }
+}
+
+function getTimestamp() {
+    let date = new Date()
+    let res = date.getFullYear().toString() + "-" +
+        date.getMonth().toString() + "-" +
+        date.getDay().toString() + " " +
+        date.getHours().toString() + ":" +
+        date.getMinutes().toString() + ":" +
+        date.getSeconds().toString() + ":" +
+        date.getMilliseconds().toString()
+    return res
+}
+
+function getIdChat(id1, id2){
+    return Math.min(id1, id2).toString() + ":" + Math.max(id1, id2).toString()
+}
+
+var db = new Db()
+
+module.exports = {db, Message}
